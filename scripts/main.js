a043205c3c325b68b0001f5263c3b3d7a3f06f09
Rafael Andrade <rsandrade90@users.noreply.github.com>
2021-08-23 21:49:58 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector('img');
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png'){
+        myImage.setAttribute('src','images/chrome.jpg');
+    } else {
+        myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
+
+let myButtom = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please, enter your name!');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', 'myName');
+        myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName;
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButtom.onclick = function() {
+    setUserName();
+}
